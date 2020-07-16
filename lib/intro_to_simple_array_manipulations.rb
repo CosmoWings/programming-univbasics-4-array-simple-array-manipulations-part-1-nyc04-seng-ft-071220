10719abf0b674831f0c32f10784566995f080265
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-16 18:14:24 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(arr,str)
+  arr.push(str)
+end
+
+def using_unshift(arr,str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  popped_element = arr.pop()
+  popped_element
+end
+
+def pop_with_args(arr)
+  
+end
